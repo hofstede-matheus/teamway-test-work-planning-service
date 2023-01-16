78bae4ffab7dd9030e13a71862958ed2b0086f44
hofstede-matheus <hofstede.matheus@gmail.com>
2023-01-16 18:03:46 -0300

    feat(Remove Worker): implement remove user flow

--- src/interactors/usecases/RemoveWorkerUsecase.ts
+++ src/interactors/usecases/RemoveWorkerUsecase.ts
@@ -1,8 +1,10 @@
 import { Inject, Injectable } from '@nestjs/common';
+import { WorkerEntity } from '../../domain/entities/Worker.entity';
 import { WorkerRepository } from '../../domain/repositories/WorkerRepository';
-import { Either } from '../../shared/helpers/either';
+import { Either, left, right } from '../../shared/helpers/either';
 import { DomainError } from '../../shared/helpers/errors';
 import { UseCase } from '../../shared/helpers/usecase';
+import { Validator } from '../../shared/helpers/validator';
 
 @Injectable()
 export class RemoveWorkerUsecase implements UseCase {
@@ -10,7 +12,13 @@ export class RemoveWorkerUsecase implements UseCase {
     @Inject(WorkerRepository)
     private workerRepository: WorkerRepository,
   ) {}
-  execute(id: string): Promise<Either<DomainError, void>> {
-    throw new Error('Method not implemented.');
+
+  async execute(id: string): Promise<Either<DomainError, void>> {
+    const validation = Validator.validate({ id: [id] });
+    if (validation.isLeft()) return left(validation.value);
+
+    await this.workerRepository.remove(id);
+
+    return right();
   }
 }
