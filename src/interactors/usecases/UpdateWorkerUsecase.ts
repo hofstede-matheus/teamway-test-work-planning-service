8d410ee6efcf99dae0e4ad95fd22f8a5854a36df
hofstede-matheus <hofstede.matheus@gmail.com>
2023-01-16 17:00:06 -0300

    feat(Worker Update): implement UpdateWorkerUsecase flow

--- src/interactors/usecases/UpdateWorkerUsecase.ts
+++ src/interactors/usecases/UpdateWorkerUsecase.ts
@@ -1,9 +1,10 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { WorkerEntity } from '../../domain/entities/Worker.entity';
 import { WorkerRepository } from '../../domain/repositories/WorkerRepository';
-import { Either } from '../../shared/helpers/either';
+import { Either, left, right } from '../../shared/helpers/either';
 import { DomainError } from '../../shared/helpers/errors';
 import { UseCase } from '../../shared/helpers/usecase';
+import { Validator } from '../../shared/helpers/validator';
 
 @Injectable()
 export class UpdateWorkerUsecase implements UseCase {
@@ -11,10 +12,21 @@ export class UpdateWorkerUsecase implements UseCase {
     @Inject(WorkerRepository)
     private workerRepository: WorkerRepository,
   ) {}
-  execute(
+  async execute(
     workerId: string,
     updateFields: Pick<Partial<WorkerEntity>, 'name'>,
   ): Promise<Either<DomainError, WorkerEntity>> {
-    throw new Error('Method not implemented.');
+    const entityValidation = WorkerEntity.build(updateFields.name);
+    if (entityValidation.isLeft()) return left(entityValidation.value);
+
+    const validation = Validator.validate({ id: [workerId] });
+    if (validation.isLeft()) return left(validation.value);
+
+    const worker = await this.workerRepository.update({
+      ...updateFields,
+      id: workerId,
+    });
+
+    return right(worker);
   }
 }
