e593d7f72b06a46c94cb3940109fe202f35e9f8f
hofstede-matheus <hofstede.matheus@gmail.com>
2023-01-18 11:33:40 -0300

    chore: add SwaggerCustomOptions type to DOCS_OPTIONS

--- src/presentation/http/docs/swagger.ts
+++ src/presentation/http/docs/swagger.ts
@@ -1,5 +1,7 @@
+import { SwaggerCustomOptions } from '@nestjs/swagger';
+
 export const DOCUMENT_TITLE = 'teamway-test-work-planning-service API';
 export const DOCUMENT_DESCRIPTION =
   'API for teamway-test-work-planning-service';
 export const DOCS_PATH = 'docs';
-export const DOCS_OPTIONS = {};
+export const DOCS_OPTIONS: SwaggerCustomOptions = {};
