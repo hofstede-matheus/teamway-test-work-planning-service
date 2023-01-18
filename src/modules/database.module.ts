248f0ee589e33ecb50eb24452b89fdc6ec2ef4c8
hofstede-matheus <hofstede.matheus@gmail.com>
2023-01-18 13:56:54 -0300

    refact: refactor database connection to use DatabaseModule

---
+++ src/modules/database.module.ts
@@ -0,0 +1,28 @@
+import { DynamicModule, ForwardReference, Module, Type } from '@nestjs/common';
+import { connectionSource } from '../../ormconfig';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Shift } from '../data/typeorm/entities/Shift';
+import { Worker } from '../data/typeorm/entities/Worker';
+
+export const databaseProviders: Array<
+  Type<any> | DynamicModule | Promise<DynamicModule> | ForwardReference
+> = [
+  TypeOrmModule.forRootAsync({
+    imports: [ConfigModule],
+    useFactory: async () => {
+      return {};
+    },
+    dataSourceFactory: async () => {
+      const dataSource = await connectionSource.initialize();
+      return dataSource;
+    },
+  }),
+  TypeOrmModule.forFeature([Shift, Worker]),
+];
+
+@Module({
+  imports: [...databaseProviders],
+  exports: [...databaseProviders],
+})
+export class DatabaseModule {}
